e5e79480f5bd97554a4d766d1c9d0ae9abb73676
Margarita <ivanova5619@abv.bg>
2017-08-15 18:11:09 +0100

    ready project

---
+++ resources/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    // setting-name: setting-value;
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    focusOnSelect: false,
+    initialSlide: 0,
+    pauseOnFocus: false,
+    pauseOnHover:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    //adaptiveHeight:true
+
+  });
+});
